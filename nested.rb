b761fa0515a9978b884811f379dddd898b948308
Amee Covarrubias <ameecova@yahoo.com>
2018-07-23 05:55:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -125,5 +125,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] << "Assembly"_to_i
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"._to_i
 end
